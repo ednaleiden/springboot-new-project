1be635d0a9bda248c89e78d0133f0377e61e2d37
ednaleiden <ednadupont@hotmail.com>
2022-11-30 22:38:27 -0500

    primer commit

---
+++ src/main/java/com/platzi/springboot/springfundamentos/SpringfundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.springboot.springfundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/springboot/springfundamentos/SpringfundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.springboot.springfundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
